d41eedf1372876d257953f956f55198192998130
ahsen17 <mesailedward@qq.com>
2026-08-28 21:14:23 +0800

    Add otel example.

--- src/do_as_beginner/http/controllers/system.py
+++ src/do_as_beginner/http/controllers/system.py
@@ -3,11 +3,24 @@ from typing import Any
 from django.http import HttpRequest
 from django.urls import path
 from django.urls.resolvers import URLPattern, URLResolver
+from opentelemetry import metrics, trace
+from opentelemetry.trace import SpanKind
 
 from do_as_beginner.http.base import BaseController, require_GET
 from do_as_beginner.shared import GenericResponse
 
 __all__ = ("SystemController",)
+
+
+tracer = trace.get_tracer(__name__)
+meter = metrics.get_meter(__name__)
+
+
+request_counter = meter.create_counter(
+    name="system_health_request_counter",
+    unit="requests",
+    description="Counter for system health requests",
+)
 
 
 class SystemController(BaseController):
@@ -23,7 +36,21 @@ class SystemController(BaseController):
         extra_context: Any = None,
     ) -> GenericResponse:
 
-        return GenericResponse(code=200, message="ok")
+        with tracer.start_as_current_span(
+            name="system.health",
+            kind=SpanKind.SERVER,
+        ) as span:
+            span.attribute("system.health", "ok")
+
+            request_counter.add(
+                1,
+                {
+                    "operation": "acquire system health",
+                    "result": "ok",
+                },
+            )
+
+            return GenericResponse(code=200, message="ok")
 
     def get_urls(self) -> list[URLResolver | URLPattern]:
 
